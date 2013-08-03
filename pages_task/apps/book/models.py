aa4df47ad2de91d0a4aa76c4bddfdd00acb9ddd7
Sergey Zlobov <ignitionwithin@gmail.com>
2013-08-04 02:17:04 +0300

    The Book model was added

--- pages_task/apps/book/models.py
+++ pages_task/apps/book/models.py
@@ -1,14 +1,14 @@
 from django.db import models
-from django.contrib import admin
-
-# from django.core.validators import RegexValidator
 # Create your models here.
 class TextNote(models.Model):
 	note_name  = models.CharField(max_length=20)
 	note_value = models.CharField(max_length=500)
 	note_image = models.ImageField(blank=True,upload_to='.')
+	note_book  = models.ManyToManyField('Book', blank = True, null = True)
 	def __unicode__(self):
 		return self.note_name
 
-
-admin.site.register(TextNote)
+class Book(models.Model):
+	book_name = models.CharField(max_length=20)
+	def __unicode__(self):
+		return self.book_name
