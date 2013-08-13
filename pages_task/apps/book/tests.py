ed2e0a8feb4f0d08e9f5ff3b2755881928ecfdf3
Sergey Zlobov <ignitionwithin@gmail.com>
2013-08-13 16:19:15 +0300

    Add doctrings

--- pages_task/apps/book/tests.py
+++ pages_task/apps/book/tests.py
@@ -1,40 +1,22 @@
 from django_webtest import WebTest
-from django.contrib.auth.models import User
 from models import TextNote, Book
 
 class MyTestCase(WebTest):
-
+	""" test primary project functional	"""
 	fixtures = ['fixture.json',]
-	# def setUp(self):
-	# #Users:
-	# 	self.user_1 = User.objects.create_user(
-	# 		'Chevy Chase', 'chevy@chase.com', 'chevyspassword'
-	# 	)
-	# 	self.user_2 = User.objects.create_user(
-	# 		'Jim Carrey', 'jim@carrey.com', 'jimspassword'
-	# 	)
-	# #Notes:
-	# 	self.note_1 = TextNote.objects.create(
-	# 		note_name='First', note_value='THIS is My Ten Chars',
-	# 		note_image='ss.aa'
-	# 	)
-	# 	self.note_2 = TextNote.objects.create(
-	# 		note_name='Second', note_value='THIS is My Ten Chars',
-	# 		note_image='ss.aa'
-	# 	)
-	# 	self.book_1 = Book.objects.create(
-	# 		book_name = 'Test book'
-	# 	)
 
 	def test_greeting_page(self):
+		""" test root page exists  """
 		greeting = self.app.get('/')
 		assert 'Here is my ' in greeting
 
 	def test_notes_page_exist(self):
+		""" this function tests, that pages with notes exists """
 		resp = self.app.get('/book/')
 		assert '200 OK' == resp.status
 
 	def test_add_note_exists(self):
+		""" this function covers ability to go on note's create page  """
 		resp = self.app.get('/')
 		next_step = self.app.get(resp.click('app').location)
 		form=next_step.forms['add_new_note']
@@ -42,6 +24,7 @@ class MyTestCase(WebTest):
 		assert form.submit().status == '200 OK'
 
 	def test_add_note_form_min_char_limit(self):
+		""" this function test minimum 10 chars condition """
 		resp = self.app.get('/book/create_note/')
 		forms= resp.forms
 		main_form=forms['main_form']
@@ -51,6 +34,9 @@ class MyTestCase(WebTest):
 		assert 'Ensure this value has at least 10 characters' in res
 
 	def test_add_note_form_normal_char_limit(self):
+		"""
+		this function tests ability to create note
+		"""
 		resp = self.app.get('/book/create_note/')
 		forms= resp.forms
 		main_form=forms['main_form']
@@ -60,6 +46,9 @@ class MyTestCase(WebTest):
 		assert '200 OK' == res.status
 
 	def test_custom_template_tag_exists(self):
+		"""
+		test , that required custom template tag exists, and show valid information
+		"""
 		index_page_resp  = self.app.get('/book/')
 		create_page_resp = self.app.get('/book/create_note/')
 		value_1 = str(index_page_resp.html.h4).strip('<h4>').strip('</h4>')
@@ -67,21 +56,11 @@ class MyTestCase(WebTest):
 		assert value_1 == value_2
 		assert int(value_2[-1]) and int(value_1[-1])
 
-	#	This tests don't needed with ajax image loading implimentation
-	#
-	# def test_uploaded_img_exists(self):
-	# 	index_page_resp = self.app.get('/book/').html
-	# 	img_path=index_page_resp.img['src']
-	# 	assert str(TextNote.note_image) in img_path
-	#
-	# def test_book_checkbox_exists(self):
-	# 	resp = self.app.get('/book/create_note/')
-	# 	obj = TextNote.objects.get(pk=1)
-	# 	img_path = obj.note_image
-	# 	print(str(img_path))
-	# 	assert img_path in resp.content
 
 	def test_objects_on_list_page(self):
+		"""
+		this function tests - ability to show valid notes list on note's list page
+		"""
 		page = self.app.get('/book/')
 		db_objects = TextNote.objects.all()
 		obj = db_objects[0]
--- pages_task/context_processors.py
+++ pages_task/context_processors.py
@@ -1,9 +1,15 @@
 from apps.book.models import TextNote
 def notes_amount(request):
-    try:
+	"""
+	This function create logic for task context processor.
+	Exception block tries get from database all notes, then count them.
+	Finally function returns total notes sum ini db.
+	If any record found - returns None.
+	"""
+	try:
 		obj=TextNote.objects.all()
 		notes_amount=len(obj)
-    except AttributeError:
-        obj=None
+	except AttributeError:
+		obj=None
 
-    return {'notes_amount': notes_amount,}
+	return {'notes_amount': notes_amount,}
